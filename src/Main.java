e0c07e1facfb836a7520bc5dca6eb01670d2be69
V.Zhur <lera.jur@yandex.ru>
2025-08-10 11:13:04 +0300

    bonus calculation

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int initialBalance = 100;
+        int rechargeAmount = 1100;
+        int bonus = 0;
+        int finalBalance = 0;
+
+        if (rechargeAmount > 1000) {
+            bonus = rechargeAmount / 100;
         }
+        finalBalance = initialBalance + rechargeAmount + bonus;
+
+        System.out.println(" Начальный баланс: " + initialBalance + " рублей");
+        System.out.println(" Сумма пополнения: " + rechargeAmount + " рублей");
+        System.out.println(" Начисленные бонусы: " + bonus + " рублей");
+        System.out.println(" Итоговый баланс: " + finalBalance + " рублей");
     }
 }
